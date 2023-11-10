b2a6b7c469692cf0587aa62d43be7160ef011d3c
Eric Jolibois <em.jolibois@gmail.com>
2023-11-10 22:08:21 +0100

    support custom query in domain step

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,16 @@ trait ToPrql {
 #[derive(Serialize, Deserialize, Debug)]
 struct DomainStep {
     domain: String,
+    table: bool,
 }
 
 impl ToPrql for DomainStep {
     fn to_prql(&self) -> String {
-        format!("from {}", self.domain)
+        if self.table {
+            format!("from `{}`", self.domain)
+        } else {
+            format!("from s\"{}\"", self.domain)
+        }
     }
 }
 
@@ -71,7 +76,8 @@ fn main() -> Result<()> {
         "pipeline": [
             {
                 "name": "domain",
-                "domain": "alb ums"
+                "domain": "al bums",
+                "table": true,
             }
         ],
         "dialect": "postgres"
@@ -87,19 +93,59 @@ mod tests {
     use super::*;
 
     #[test]
-    fn domain() {
+    fn domain_table() {
         let request = json!(
         {
             "pipeline": [
                 {
                     "name": "domain",
-                    "domain": "albums"
+                    "domain": "albums",
+                    "table": true,
                 }
             ],
             "dialect": "postgres"
         });
         let request: Request = serde_json::from_value(request).unwrap();
-        assert_eq!(request.to_prql(), "from albums");
-        assert_eq!(request.to_sql().unwrap(), "SELECT * FROM albums");
+        assert_eq!(request.to_prql(), "from `albums`");
+        assert_eq!(request.to_sql().unwrap(), r#"SELECT * FROM albums"#);
+    }
+
+    #[test]
+    fn domain_table_with_whitespace() {
+        let request = json!(
+        {
+            "pipeline": [
+                {
+                    "name": "domain",
+                    "domain": "al bums",
+                    "table": true,
+                }
+            ],
+            "dialect": "postgres"
+        });
+        let request: Request = serde_json::from_value(request).unwrap();
+        assert_eq!(request.to_prql(), "from `al bums`");
+        assert_eq!(request.to_sql().unwrap(), r#"SELECT * FROM "al bums""#);
+    }
+
+    #[test]
+    fn domain_custom_query() {
+        let request = json!(
+        {
+            "pipeline": [
+                {
+                    "name": "domain",
+                    "domain": "SELECT * FROM sales",
+                    "table": false,
+                }
+            ],
+            "dialect": "postgres"
+        });
+        let request: Request = serde_json::from_value(request).unwrap();
+        assert_eq!(request.to_prql(), r#"from s"SELECT * FROM sales""#);
+        assert_eq!(
+            request.to_sql().unwrap(),
+            "WITH table_0 AS (SELECT * FROM sales) SELECT * FROM table_0"
+        );
     }
 }
